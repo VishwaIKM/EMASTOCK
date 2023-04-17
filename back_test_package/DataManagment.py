0444e1e0b31a7a021f99f51119b25b7253ad53c1
Vishwa Pratap Singh <Vishwa@gmail.com>
2023-04-18 00:39:50 +0530

    testing

--- back_test_package/DataManagment.py
+++ back_test_package/DataManagment.py
@@ -1,34 +1,40 @@
 # THIS SCRIPT WILL PROVIDE THE DATA FRAME ACCORDING TO SYMBOL AND DATE RANGE
-import datetime
-import os
+from datetime import datetime
 import pandas as pd
 
 # var
-Path = "D:/WorkSpace/BackTestProject/DataBase/"  # for time being this is fix will figure out later if as be will
+Path = "C:/Users/vishw/OneDrive/Documents/WORKSPACE/back_test_project/DataBase/"  # for time being this is fix will
+
+
+# figure out later if as be will
 # move the database to Sql later on.
 
 
 class DataBaseUpdate:
-    DataFrameSymbolFilter = None
-    DataFrameDateRangeFilter = None
-    DataFrameDayWiseFilter = None
+    DataFrameSymbolFilter: pd.DataFrame = None
+    DataFrameDateRangeFilter: pd.DataFrame = None
+    DataFrameDayWiseFilter: pd.DataFrame = None
 
     @classmethod
-    def gui_symbol_and_date_change(cls, symbol_name: str, start_date: datetime, end_date: datetime):
+    def gui_symbol_and_date_change(cls, symbol_name: str, start_date: datetime, end_date: datetime) -> pd.DataFrame:
+        # print(f"SD:{start_date} and ED:{end_date}")
         cls.DataFrameSymbolFilter = symbol_change(symbol_name)
         cls.DataFrameDateRangeFilter = date_range_change(start_date, end_date, cls.DataFrameSymbolFilter)
         cls.DataFrameDayWiseFilter = data_club_day_wise(cls.DataFrameDateRangeFilter)
+        return cls.DataFrameDayWiseFilter
+
+    """Below Class method for future ref if additional data manipulation required  """
 
     @classmethod
-    def data_base_updated_frame(cls):
+    def data_base_updated_frame(cls) -> pd.DataFrame:
         return cls.DataFrameDayWiseFilter
 
     @classmethod
-    def data_base_symbol_updated_frame(cls):
+    def data_base_symbol_updated_frame(cls) -> pd.DataFrame:
         return cls.DataFrameSymbolFilter
 
     @classmethod
-    def data_base_range_updated_frame(cls):
+    def data_base_range_updated_frame(cls) -> pd.DataFrame:
         return cls.DataFrameDateRangeFilter
 
 
@@ -36,15 +42,14 @@ def symbol_change(symbol_name: str) -> pd.DataFrame:
     data_base_file = Path + symbol_name + '.csv'
     df = pd.read_csv(data_base_file)
     df['Date'] = pd.to_datetime(df['Date'])
-    print(df)
     return df
 
 
 def date_range_change(start_date: datetime, end_date: datetime, df: pd.DataFrame) -> pd.DataFrame:
-    pass
+    df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+    return df
 
 
-def data_club_day_wise(dataframe: pd.DataFrame) -> pd.DataFrame:
-    pass
-
-
+def data_club_day_wise(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.groupby(['Date']).agg({'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last'})
+    return df
--- back_test_package/Indicators.py
+++ back_test_package/Indicators.py
@@ -1,25 +1,41 @@
 # THIS SCRIPT WILL PROVIDE THE INDICATOR CALCULATION
 import pandas as pd
+from abc import ABC, abstractmethod
 
 
-class ExponentialMovingAverage:
-
-    def __int__(self, data_frame: pd.DataFrame, span: int, price: str):
-        self.data_frame = data_frame
+class BaseIndicator(ABC):
+    def __int__(self, df: pd.DataFrame, span: int, price: str) -> None:
+        self.df = df
         self.span = span
         self.price = price
 
-    def ema_data_add(self) -> pd.DataFrame:
+    def start_indicator_calculation(self) -> None:
+        self.add_indicator()
+        self.base_graph(self.price)
+
+    def base_graph(self, param: str) -> pd.Series:
+        return self.df[param]
+
+    @abstractmethod
+    def add_indicator(self) -> pd.DataFrame:
         pass
 
 
-class ExponentialMovingLongShortAverage:
+class ExponentialMovingAverage(BaseIndicator):
+    def add_indicator(self) -> pd.DataFrame:
+        self.df['EMA ' + str(self.span)] = self.df[self.price].ewm(span=self.span, adjust=False).mean()
+        return self.df
 
-    def __int__(self, data_frame: pd.DataFrame, short_span: int, long_span: int, price: str):
-        self.data_frame = data_frame
-        self.short_span = short_span
+
+class ExponentialMovingLongShortAverage(BaseIndicator):
+
+    def __int__(self, df: pd.DataFrame, short_span: int, price: str,long_span: int) -> None:
+        super().__int__(df, short_span, price)
         self.long_span = long_span
-        self.price = price
 
     def ema_long_short_add(self) -> pd.DataFrame:
         pass
+
+    def add_indicator(self) -> pd.DataFrame:
+        self.df['EMA ' + str(self.span)] = self.df[self.price].ewm(span=self.span, adjust=False).mean()
+        return self.df
